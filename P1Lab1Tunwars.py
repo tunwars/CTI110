8ba983aa667202a4ba298b68b6c5ccc8d6d632ea
tunwars <90783580+tunwars@users.noreply.github.com>
2021-09-15 11:30:12 -0400

    First upload

---
+++ P1Lab1Tunwars.py
@@ -0,0 +1,13 @@
+# CTI 110
+# P1Lab1 - zylab training
+# Name
+# Date
+
+
+
+
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
